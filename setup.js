d450d41b1fe29ce2a35beedbaebaa78943529720
Anamika Nayak <40330477+anamika8@users.noreply.github.com>
2024-11-04 00:02:06 +0000

    removing headless option

--- setup.js
+++ setup.js
@@ -1,13 +1,8 @@
 const { Builder } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 
-const chromeOptions = new chrome.Options();
-chromeOptions.addArguments('--headless');
-
-
 const driver = new Builder()
   .forBrowser('chrome')
-  .setChromeOptions(chromeOptions)
   .build();
 
 module.exports = { driver };
